9ced35d4d714993edc1f3c81298c91b07f5bd6f8
Natalia Grusha <gruscha.natascha2017@yandex.ru>
2022-09-24 19:49:22 +0300

    Замена JS

--- scripts/index.js
+++ scripts/index.js
@@ -18,43 +18,33 @@ const itemPlaceElement = document.querySelector('.items-grid');
 const itemTableElement = itemPlaceElement.querySelector('.items-grid__table');
 const popupItemCloseButtonElement = popupItemElement.querySelector('.popup__close_item'); 
 const popupItemFormElement = popupItemElement.querySelector('.popup__content_item');
-const popupItemInputNameElement = popupItemFormElement.querySelector('.popup__input_type_name');
+const popupItemInputTitleElement = popupItemFormElement.querySelector('.popup__input_type_title');
 const popupItemInputLinkElement = popupItemFormElement.querySelector('.popup__input_type_link');
 const templateItemElement = document.querySelector('.item-template').content;
-const cardItemElement = document.querySelector('.items-grid__card');
-const cardItemPhotoElement = document.querySelector('.items-grid__photo');
-const deleteElement = document.querySelector('.items-grid__delete');
-const cardItemNameElement = document.querySelector('.items-grid__title');
-const cardItemLikeElement  = document.querySelector('.items-grid__like');
+const popupItemButtonElement = document.querySelector('.popup__submit-btn_item');
 
-//добавление карточек items//внесение значений place и отправка данных
-const items = [itemPlaceElement];
+//добавление карточек items
+const items = [
+	{
+		name: 'Челябинская область',
+		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+	}
+];
 
 function render() {
     items.forEach(renderItem);
 }
-function createItem(name, link) {
+function renderItem(cardData) {
     const newItem = templateItemElement.cloneNode(true);
-    cardItemNameElement.textContent = name;
-    cardItemNameElement.alt = name;
-    cardItemPhotoElement.src = link;
-    return newItem;
+    newItem.querySelector('.items-grid__photo').src = cardData.link;
+    newItem.querySelector('.items-grid__title').innerText = cardData.name;
+    itemTableElement.appendChild(newItem);
 };
-
-profilePlusButtonElement.addEventListener('click', renderItem);
-//cardItemLikeElement.addEventListener('click', likeItem);
-cardItemPhotoElement.addEventListener('click', photoOpen);
-
-function renderItem(name, link) {
-    items.prepend(createItem(name, link));
-}
-
+popupItemFormElement.addEventListener('submit', function cardData(link, name) {
+    link = popupItemInputLinkElement;
+    name = popupItemInputTitleElement;
+});
 render();
-
-//Like item
-//function likeItem(){
-//    cardItemLikeElement.src = 
-//}
 
 //открытие popup "редактирование профиля" и "добавление карточек":
 const buttonarray = [popupElement, popupItemElement]
@@ -62,6 +52,7 @@ const popupOpen = function(index) {
     buttonarray[index].classList.toggle('popup_opened');
     popupInputNameElement.textContent = profileNameElement.value;
     popupInputJobElement.textContent = profileJobElement.value;
+
 };
 profileOpenButtonElement.addEventListener('click', ()=> popupOpen(0));
 profilePlusButtonElement.addEventListener('click', ()=> popupOpen(1));
@@ -74,19 +65,6 @@ const popupClose = function() {
 popupCloseButtonElement.addEventListener('click', popupClose);
 popupItemCloseButtonElement.addEventListener('click', popupClose);
 
-//Выборка DOM елеметов для Zoom
-const itemZoomElement = document.querySelector('.popup__zoom');
-const itemZoomContainerElement = document.querySelector('.popup__zoom_container');
-const itemZoomImgElement = document.querySelector('.popup__zoom_img');
-const itemZoomTitleElemnt = document.querySelector('.popup__zoom_title');
-
-//Zoomphoto
-function photoOpen(el) {
-    itemZoom.classList.add('popup_opened');
-    itemZoomImgElement.src = el.currentTarget.closest('.items-grid__photo').src;
-    itemZoomTitleElemnt.textContent = el.currentTarget.closest('.items-grid__title').alt;
-}
-
 //Изменение значений в шапке профиля
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -96,6 +74,7 @@ function formSubmitHandler (evt) {
     profileJobElement.textContent = jobElement;
     popupClose();
 }
+
 popupFormElement.addEventListener('submit', formSubmitHandler);
 
 function formKeypressHandler (e) {
@@ -107,12 +86,4 @@ function formKeypressHandler (e) {
 popupInputNameElement.addEventListener('keypress', formKeypressHandler);
 popupInputJobElement.addEventListener('keypress', formKeypressHandler);
 popupItemInputLinkElement.addEventListener('keypress', formKeypressHandler);
-popupItemInputNameElement.addEventListener('keypress', formKeypressHandler);
-
-//удаление items
-function addItems(el) {
-    el.querySelector('.item-grid__button').addEventListener('click', handleDelete);
-};
-function handleDelete(event) {
-   event.target.closest('.items-grid__card').remove();
-}
+popupItemInputTitleElement.addEventListener('keypress', formKeypressHandler);
